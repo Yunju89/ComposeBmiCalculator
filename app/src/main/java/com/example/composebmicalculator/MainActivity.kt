26ff9ca1726835864ec6bac45dfb36152c261248
Yunju <yunjoo1018@gmail.com>
2023-04-24 17:49:36 +0900

    비만도 계산기 Composable 화면 구현

--- app/src/main/java/com/example/composebmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/composebmicalculator/MainActivity.kt
@@ -3,22 +3,70 @@ package com.example.composebmicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composebmicalculator.ui.theme.ComposeBmiCalculatorTheme
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            HomeScreen()
         }
     }
 }
 
+@Composable
+fun HomeScreen() {
+    val (height, setHeight) = rememberSaveable {
+        mutableStateOf("")
+    }
 
+    val (weight, setWeight) = rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Scaffold(           // TopAppBar 구현 하기 위해서 Scaffold 감싸기
+        modifier = Modifier.fillMaxSize(),
+        topBar = {      // topBar : () -> Unit  함수 형태 이기 때문에, {} 내부에 작성
+            TopAppBar(  // TopAppBar(title=: () -> Unit)  함수 형태 이기 때문에, title {} 내부에 작성
+                title = { Text(text = "비만도 계산기") }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)        // 컴포즈 1.2.0부터는 컴포저블에 전달 되는 패딩 매개 변수를 사용 해야 한다.
+                .padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = height,
+                onValueChange = setHeight,
+                label = { Text("키") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            )
+            OutlinedTextField(
+                value = weight,
+                onValueChange = setWeight,
+                label = { Text("몸무게") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                modifier = Modifier.align(Alignment.End),
+                onClick = {}) {
+                Text(text = "결과")
+            }
+        }
+    }
+}
