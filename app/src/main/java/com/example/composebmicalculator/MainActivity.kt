0792ff825c9070450b49603c829762919dfabc7f
Yunju <yunjoo1018@gmail.com>
2023-04-25 09:32:07 +0900

    두번째 Composable 화면 구현

--- app/src/main/java/com/example/composebmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/composebmicalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.composebmicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -11,8 +12,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +50,7 @@ fun HomeScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it)        // 컴포즈 1.2.0부터는 컴포저블에 전달 되는 패딩 매개 변수를 사용 해야 한다.
+                .padding(it)        // 컴포즈 1.2.0부터는 컴포저블에 전달 되는 패딩 매개 변수를 사용 해야 한다. 최 상위 컨테이너 / 뷰에 적용 한다.
                 .padding(16.dp)
         ) {
             OutlinedTextField(
@@ -69,4 +75,40 @@ fun HomeScreen() {
             }
         }
     }
+}
+
+@Composable
+fun ResultScreen(bmi: Double) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "비만도 계산기") }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "과체중", fontSize = 30.sp)
+            Spacer(modifier = Modifier.height(50.dp))
+            Image(
+                painter = painterResource(id = R.drawable.baseline_sentiment_dissatisfied_24),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp),
+                colorFilter = ColorFilter.tint(
+                    color = Color.Black
+                )
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Preview(){
+    ResultScreen(bmi = 35.0)
 }
