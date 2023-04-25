44f773580b36a7fc97a93802a8867901726ee038
Yunju <yunjoo1018@gmail.com>
2023-04-25 10:20:23 +0900

    화면 전환 내비게이션 활용 및 ViewModel 사용하여 로직 작성

--- app/src/main/java/com/example/composebmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/composebmicalculator/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -19,25 +20,34 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlin.math.pow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel = viewModel<BmiViewModel>()
             val navController = rememberNavController() // navController 객체
+
+            val bmi = viewModel.bmi.value
+
             NavHost(navController = navController, startDestination = "home") {  // 화면 구성
                 composable(route = "home") {
-                    HomeScreen(navController)
+                    HomeScreen() {height, weight ->
+                        viewModel.bmiCalculate(height, weight)
+                        navController.navigate("result")
+                    }
                 }
                 composable(route = "result") {
-                    ResultScreen(navController, bmi = 35.0)
+                    ResultScreen(navController, bmi = bmi)
                 }
             }
         }
@@ -45,7 +55,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeScreen(navController: NavController) {
+fun HomeScreen(
+    onResultClicked : (Double, Double) -> Unit
+    ) {
     val (height, setHeight) = rememberSaveable {
         mutableStateOf("")
     }
@@ -86,7 +98,9 @@ fun HomeScreen(navController: NavController) {
             Button(
                 modifier = Modifier.align(Alignment.End),
                 onClick = {
-                    navController.navigate("result")
+                    if(height.isNotEmpty() && weight.isNotEmpty()){
+                        onResultClicked(height.toDouble(), weight.toDouble())
+                    }
                 }) {
                 Text(text = "결과")
             }
@@ -96,6 +110,21 @@ fun HomeScreen(navController: NavController) {
 
 @Composable
 fun ResultScreen(navController: NavController, bmi: Double) {
+    val result = when{
+        bmi >= 35 -> "고도 비만"
+        bmi >= 30 -> "2단계 비만"
+        bmi >= 25 -> "1단계 비만"
+        bmi >= 23 -> "과체중"
+        bmi >= 18.5 -> "정상"
+        else -> "저체중"
+    }
+
+    val imageRes = when{
+        bmi >= 23 -> R.drawable.baseline_sentiment_very_dissatisfied_24
+        bmi >= 18.5 -> R.drawable.baseline_sentiment_satisfied_24
+        else -> R.drawable.baseline_sentiment_dissatisfied_24
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -117,10 +146,10 @@ fun ResultScreen(navController: NavController, bmi: Double) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "과체중", fontSize = 30.sp)
+            Text(text = result, fontSize = 30.sp)
             Spacer(modifier = Modifier.height(50.dp))
             Image(
-                painter = painterResource(id = R.drawable.baseline_sentiment_dissatisfied_24),
+                painter = painterResource(imageRes),
                 contentDescription = null,
                 modifier = Modifier.size(100.dp),
                 colorFilter = ColorFilter.tint(
@@ -128,5 +157,14 @@ fun ResultScreen(navController: NavController, bmi: Double) {
                 )
             )
         }
+    }
+}
+
+class BmiViewModel : ViewModel() {
+    private val _bmi = mutableStateOf(0.0)
+    val bmi: State<Double> = _bmi                   // State 값이 변경 될 경우 화면이 다시 그려짐 -> 값 자동으로 갱신 됨
+
+    fun bmiCalculate(height: Double, weight: Double) {
+        _bmi.value = weight / (height / 100.0).pow(2.0)
     }
 }
